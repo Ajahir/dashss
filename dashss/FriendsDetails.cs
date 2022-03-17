32df00dc70c9dd5decb41ec8fcac669241320a16
Hp <Hp@DESKTOP-H5VIABE>
2022-03-17 08:03:44 +0530

    Add project files.

---
+++ dashss/FriendsDetails.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dashss
+{
+    class Friends
+    {
+        public Friends(int id, string name, string description,string moradetail)
+        {
+            PhotoId = id;
+            Fname = name;
+            Fdescription = description;
+            Moradetail = moradetail;
+
+        }
+
+        public int PhotoId { get; }
+        public string Fname { get; }
+        public string Fdescription { get; }
+        public string Moradetail { get; }
+    }
+    class FriendsDetails
+    {
+        static Friends[] frnd = {
+
+            new Friends(Resource.Drawable.facebok, "Ross Geller", "The Dinosour Guy", "great"),
+            new Friends(Resource.Drawable.facebok, "Rachel Green", "Raquel", "good"),
+            new Friends(Resource.Drawable.facebok, "Chandler Bing", "Miss Chanandler Bong", "ecelent"),
+            new Friends(Resource.Drawable.facebok, "Monica Geller", "The Chef", "gog"),
+            new Friends(Resource.Drawable.facebok, "Joey Tribbiani", "Kan Adams", "jhh"),
+        };
+        private Friends[] mfrnd;
+
+        Random random;
+
+        public FriendsDetails()
+        {
+            mfrnd = frnd;
+            random = new Random();
+        }
+
+        public int NumbFriends
+        {
+
+            get { return mfrnd.Length; }
+        }
+
+        public Friends this[int i]
+        {
+            get { return mfrnd[i]; }
+        }
+
+        public int RandomSwap()
+        {
+            Friends tempfrnd = mfrnd[0];
+
+            int mrandom = random.Next(1, mfrnd.Length);
+
+            mfrnd[0] = mfrnd[mrandom];
+            mfrnd[mrandom] = tempfrnd;
+
+            return mrandom;
+        }
+
+        public void Suffle()
+        {
+            for (int i = 0; i < mfrnd.Length; i++)
+            {
+                Friends tempfrnd = mfrnd[i];
+
+
+                int mrandom = random.Next(1, mfrnd.Length);
+
+                mfrnd[i] = mfrnd[mrandom];
+                mfrnd[mrandom] = tempfrnd;
+            }
+
+        }
+    }
+}
---
+++ dashss/MyAdapter.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dashss
+{
+    class MyAdapter : RecyclerView.Adapter
+    {
+        private FriendsDetails _friendsDetails;
+        private Context context;
+
+        public MyAdapter(FriendsDetails friendsDetails, Context context)
+        {
+            _friendsDetails = friendsDetails;
+            this.context = context;
+        }
+
+        public override int ItemCount => _friendsDetails.NumbFriends;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            MyViewHolder viewHolder = holder as MyViewHolder;
+            viewHolder.Binddata(_friendsDetails, position);
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.recycleviewdata, parent, false);
+            return new MyViewHolder(view);
+        }
+    }
+    class MyViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView friendImage;
+        public TextView friendNameText;
+        public TextView friendDescriptionText;
+        public TextView friendmoradetailText;
+
+        public RelativeLayout relativeLayout2;
+        public MyViewHolder(View itemView) : base(itemView)
+        {
+            friendImage = itemView.FindViewById<ImageView>(Resource.Id.imageWhishlist);
+            friendNameText = itemView.FindViewById<TextView>(Resource.Id.textView1);
+            friendDescriptionText = itemView.FindViewById<TextView>(Resource.Id.textView2);
+            friendmoradetailText= itemView.FindViewById<TextView>(Resource.Id.textView3);
+
+
+
+
+
+
+        }
+
+        
+
+        internal void Binddata(FriendsDetails friendsDetails, int position)
+        {
+            friendImage.SetImageResource(friendsDetails[position].PhotoId);
+            friendNameText.Text = friendsDetails[position].Fname;
+            friendDescriptionText.Text = friendsDetails[position].Fdescription;
+            friendmoradetailText.Text = friendsDetails[position].Moradetail;
+        }
+    }
+}
